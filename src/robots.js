c1bb089256d157f85534fe4896143cba05eb2ea9
Adrian Avram <avram.adrian.sorin@hotmail.co.uk>
2018-09-14 00:28:50 +0300

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+	{
+		id: 1,
+		name: "First Robot",
+		username: "first",
+		email: "first.robot@mailinator.com"
+	},
+	{
+		id: 2,
+		name: "Second Robot",
+		username: "second",
+		email: "second.robot@mailinator.com"
+	},
+	{
+		id: 3,
+		name: "Third Robot",
+		username: "third",
+		email: "third.robot@mailinator.com"
+	},
+	{
+		id: 4,
+		name: "Fourth Robot",
+		username: "fourth",
+		email: "fourth.robot@mailinator.com"
+	},
+	{
+		id: 5,
+		name: "Fifth Robot",
+		username: "fifth",
+		email: "fifth.robot@mailinator.com"
+	},
+	{
+		id: 6,
+		name: "Sixth Robot",
+		username: "sixth",
+		email: "sixth.robot@mailinator.com"
+	}
+];
