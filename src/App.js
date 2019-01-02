e37d2186139b4c2aaacd64c44db731b77db39556
Adrian Avram <avram.adrian.sorin@hotmail.co.uk>
2019-01-02 19:54:50 +0200

    fix linting

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
-import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: robots,
-			searchfield: ""
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: ''
+    };
+  }
 
-	onSearchChange = event => {
-		this.setState({ searchfield: event.target.value });
-	};
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ robots: users });
+      });
+  }
 
-	render() {
-		const filteredRobots = this.state.robots.filter(robots => {
-			return robots.name
-				.toLowerCase()
-				.includes(this.state.searchfield.toLowerCase());
-		});
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-		return (
-			<div className="tc">
-				<h1>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={filteredRobots} />
-			</div>
-		);
-	}
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>
+      robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    );
+
+    if (robots.length === 0) {
+      return <h1>Loading</h1>;
+    }
+
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
